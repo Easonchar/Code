81b6ea9faba02c25a5452cda33c475bec5f5c2ee
Easonchar <1009560311@qq.com>
2018-10-27 16:45:22 +0800

    创建项目并修改路径

--- MyDjango/MyDjango/settings.py
+++ MyDjango/MyDjango/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'MyDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'leshangwang',
+        'USER':'root',
+        'PASSWORD':'666666',
+        'PORT':'3306',
+        'HOST':'localhost'
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'Asia'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -119,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
