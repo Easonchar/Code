2d9280a1ce62c4e2e824082137de63a6faeba1af
Easonchar <1009560311@qq.com>
2018-10-29 10:44:37 +0800

    项目创建

--- MyDjango/App/urls.py
+++ MyDjango/App/urls.py
@@ -4,5 +4,11 @@ from App import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
+    url(r'^detail/$',views.detail,name='detail'),
+    url(r'^list/$',views.list,name='list'),
+    url(r'^login/$',views.login,name='login'),
+    url(r'^register/$',views.register,name='register'),
+    url(r'^shoppingCar/$',views.shoppingCar,name='shoppingCar'),
+
 
 ]
